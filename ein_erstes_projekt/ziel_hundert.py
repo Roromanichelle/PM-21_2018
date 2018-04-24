1ef0ecea5b85db6c11a124e3ee33cfd8d166494b
Romane Carette <carette.romane@gmail.com>
2018-04-24 22:29:37 +0200

    Verbesserungsvorschlagen Romane C

--- ein_erstes_projekt/ziel_hundert.py
+++ ein_erstes_projekt/ziel_hundert.py
@@ -4,7 +4,7 @@ import random    # wird in der Funktion zahl_waehlen benötigt
 
 
 # ++++++++++++++++++++++++++++++++++++
-#
+
 # Funktionsdefinitionen:
 # Aufruf erfolgt aus dem Hauptprogramm
 #
@@ -14,22 +14,23 @@ def zahl_vom_spieler_holen():
     
     Das ganze hier ist ein docstring.
     """
-    
+    i = True
     # solange nachfragen bis eine gültige Zahl eingegeben wird
-    while True:
+    while i:
         zahl_als_text = input("Wieviel möchten Sie addieren?")
         zahl = int(zahl_als_text)
         if zahl < 1 or zahl > 10:
             print("Bitte eine Zahl von 1-10 eingeben")
         else:
-            return zahl
+            i=False
+    return zahl
 
 
-def zahl_waehlen():
+def zahl_waehlen( e = 10 ) :
     """Liefert die nächste Zahl, die der Computer addiert."""
     
     # TO DO: die KI nochmal überdenken!
-    return random.randint(1, 10)
+    return random.randint(1, e)
 
 
 # ++++++++++++++++++++++++++++++++++++
@@ -37,7 +38,7 @@ def zahl_waehlen():
 # Hier beginnt das Hauptprogramm
 #
 
-print("                 ZIEL HUNDERT")
+print(" ZIEL HUNDERT")
 print()
 print("Bei jedem Zug ist eine Zahl zwischen 1 und 10 zur bisherigen")
 print("Gesamtzahl zu addieren.")
@@ -45,27 +46,28 @@ print("Wer zuerst 100 erreicht, gewinnt.")
 print()
 
 # Startzahl s und Ziel e festlegen
-s = 1
+s = zahl_waehlen( 30 )
 e = 100
+i=True
 
 # Spiele los!
 print("Startzahl:", s)
 print()
 
 # Wenn s den Wert von e erreicht, wird abgebrochen und der Gewinner ausgegeben
-while True:
+while i:
     # Zug des menschlichen Spielers
     # Zahl z von der Eingabe einlesen
     z = zahl_vom_spieler_holen()
     # z zur momentanen Summe s addieren
-    s = s + z
+    s = s + z 
     print()
     print("Neuer Stand:", s)
     print()
     # Gewinnbedingung prüfen und ggf. abbrechen
     if s >= e:
         print("Sie haben gewonnen :(")
-        break
+        i=False
     
     # Computerzug
     # Zahl z wählen
@@ -75,4 +77,6 @@ while True:
     print("Neuer Stand", s)
     if s >= e:
         print("Ha, ich habe gewonnen!")
-        break
+        i=False
+
+
